cb953484ae1122a1953fceece61980f1f6eaba3d
Yiyi Sun <yiyisun@gmail.com>
2019-01-16 09:41:22 -0500

    initialize project

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import app from 'apprun';
+
+const model = 'Hello world - AppRun !';
+
+const view = (state) => <div>
+  <h1>{state}</h1>
+</div>;
+
+const update = {
+
+}
+
+app.start('my-app', model, view, update);
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: {
+    'dist/app': './src/main.tsx',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /.tsx?$/, loader: 'ts-loader' },
+      { test: /\.js$/, use: ["source-map-loader"], enforce: "pre" }
+    ]
+  },
+  devServer: {
+    open: true
+  },
+  devtool: 'source-map'
+}
